405833ae0ce437f69f79163af7cac0819d1692d8
Krishnakumar <93043237+krishkumar12@users.noreply.github.com>
2022-11-30 23:45:36 +0530

    Update mongodb.py

--- src/database/mongodb.py
+++ src/database/mongodb.py
@@ -8,11 +8,11 @@ ca = certifi.where()
 class MongodbOperation:
 
     def __init__(self) -> None:
-        #self.client = "mongodb+srv://krishna12:krishna12mongodb@krish12.hgjeeww.mongodb.net/?retryWrites=true&w=majority"
-        self.client = pymongo.MongoClient("mongodb+srv://krishna12:krishna12mongodb@krish12.hgjeeww.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=ca)
+        #self.client = ""
+        self.client = ""
         
         #self.client = pymongo.MongoClient(os.getenv('MONGO_DB_URL'),tlsCAFile=ca)
-        self.db_name="krish12"
+        self.db_name=" "
 
     def insert_many(self,collection_name,records:list):
         self.client[self.db_name][collection_name].insert_many(records)
